6f6354f399308912855bfc2ff5de6b7bb9fe0102
Nate Wolfe <nate@wxlfe.dev>
2021-12-12 15:06:30 -0500

    Removed Unnecessary Imports

--- src/main/frontend/src/components/UserProfile/UserProfile.js
+++ src/main/frontend/src/components/UserProfile/UserProfile.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './UserProfile.css';
 import PageNotFound from '../PageNotFound';
